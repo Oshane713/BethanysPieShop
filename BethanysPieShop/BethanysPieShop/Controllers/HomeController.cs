200a1fe134162fa853cc55d07cba234e28ea117f
Oshane McDonald <oshane713@gmail.com>
2020-09-12 19:46:29 -0500

    Initial commit

---
+++ BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModel;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRespository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRespository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRespository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+             new List<Category>
+             {
+                new Category{CategoryID=1, CategoryName = "Fruit Pies", Description = "All fruity pies"},
+                new Category{CategoryID=2, CategoryName = "Cheese cakes", Description = "Cheesy all the way"},
+                new Category{CategoryID=3, CategoryName = "Seasonal Pies", Description = "Get in the mood for a seasonal pie"}
+             };
+    }
+}
